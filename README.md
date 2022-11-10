03d3258eada7b36345e3e1202cd13d476a88e110
Tam2412 <tamle.dev@gmail.com>
2022-11-11 00:30:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Tìm hiểu về React
+
+5.1 JSX
+
+- JSX là gì ?
+- Tại sao lại sử dụng JSX ?
+- Một số tiện ích
+
+  5.2 Component
+
+=> Khi app chạy sẽ build Class Component => return ra những thẻ HTML vào Virtual DOM => Virtual DOM sẽ đi so sánh với Real DOM => Check những thành phần
+nào thay đổi thì sẽ mounting vào DOM.
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import CounterApp from "./component/CounterApp";
+
+function App() {
+  const [show, setShow] = useState(false);
+
+  const showCounterApp = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div>
+      <button onClick={showCounterApp}>Show App</button>
+      {show ? <CounterApp /> : <h3>Counter app not display!</h3>}
+    </div>
+  );
+}
+
+export default App;
+
+// JSX => Javascript XML
+// JSX => Sử dụng thay thế react.CreateElement => thân thiện hơn với dev FE
+// JSX => Có thể nhúng code JS vào, biến, biểu thức, hàm ,...
